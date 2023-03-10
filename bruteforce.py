9b56a60819baf057c9936d1ba3413c5cc7bb8d7e
Papiexx <boutdepapier27@outlook.fr>
2023-03-10 11:26:35 +0100

    changes the way of results printed

--- bruteforce.py
+++ bruteforce.py
@@ -1,44 +1,51 @@
 import itertools
 import sys
+from time import perf_counter
 
 from utils import read_actions_csv
 
 
 def get_best_combinations(csv_file):
     """get the best combinations with bruteforce"""
-
+    start_time = perf_counter()
     # Dictionnaire d'actions
     actions = read_actions_csv(csv_file)
 
     # Ajouter le bénéfice pour chaque action
     for action in actions:
         price = actions[action]["price"]
-        profit = actions[action]["profit"]
-        benefit = price * profit / 100
+        percentage = actions[action]["percentage"]
+        benefit = price * percentage / 100
         actions[action]["benefit"] = benefit
 
-    max_budget = 500
-
-
     # Trouver la combinaison optimale d'actions
+    max_budget = 500
     max_profit = 0
     max_combination = ()
     count = 0
+    optimal_price = 0
 
     for actions_number in range(1, len(actions) + 1):
         for combination in itertools.combinations(actions.keys(), actions_number):
             count += 1
+            #print(count, end="\r")
             total_price = sum([actions[action]["price"] for action in combination])
             if total_price <= max_budget:
                 total_benefit = sum([actions[action]["benefit"] for action in combination])
                 if total_benefit > max_profit:
                     max_profit = total_benefit
                     max_combination = combination
+                    optimal_price = total_price
 
-    # Afficher la combinaison optimale et le bénéfice total et le nombre de combinaisons testée
-    print("Combinaison optimale d'actions: ", max_combination)
-    print(f"Pour un total de {count} combinaisons possible")
-    print("Bénéfice total: ", round(max_profit, 2), "euros")
+    elapsed_time = perf_counter() - start_time
+
+    print(
+        f"[Dépense totale: {optimal_price}€ sur {max_budget}€] \n"
+        f"[Bénéfice total: {round(max_profit, 2)}€] \n"
+        f"[Temps d'éxécution: {elapsed_time:.3f}] \n"
+        f"[Nombre de combinaisons: {count}] \n"
+        f"Avec cette liste d'action: \n {max_combination}"
+    )
 
 
 if __name__ == "__main__":
