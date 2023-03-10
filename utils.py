eba509c28f6a14bc0a703a33fb9dbd93fdde7024
Papiexx <boutdepapier27@outlook.fr>
2023-03-10 11:24:24 +0100

    modify read_csv with cleaning operation

--- utils.py
+++ utils.py
@@ -6,10 +6,44 @@ def read_actions_csv(csv_file) -> dict:
     actions = {}
     with open(csv_file, newline="") as csvfile:
         reader = csv.reader(csvfile, delimiter=",", quotechar='"')
+        if do_we_skip_the_first_line(csv_file):
+            next(reader)
         for row in reader:
+            if is_null_or_not(row):
+                continue
             action_name = row[0]
-            price = int(row[1])
-            profit = float(row[2].replace(",", "."))
-            actions[action_name] = {"price": price, "profit": profit}
+            price = float(row[1])
+            percentage = float(row[2])
+            actions[action_name] = {"price": price, "percentage": percentage}
 
     return actions
+
+
+def print_action_by_action(actions, message):
+    """"""
+    print(message)
+    for key, value in actions.items():
+        print(key, value)
+
+
+def do_we_skip_the_first_line(csv_file):
+    """if first line of csv contains strings like 'profit' etc... skip the first line"""
+    with open(csv_file, newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",", quotechar='"')
+        for row in reader:
+            if row[0] == "name" or row[1] == "price" or row[2] == "profit":
+                return True
+            else:
+                return False
+
+
+def is_null_or_not(row):
+    """determine and pass the row if value == 0 or negative"""
+    row[1] = float(row[1])
+    row[2] = float(row[2])
+    if float(row[1]) == 0.0 or float(row[2]) == 0.0:
+        return True
+    elif float(row[1]) < 0 or float(row[2]) < 0:
+        return True
+    else:
+        return False
